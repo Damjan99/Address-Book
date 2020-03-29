32ec06893864f1d8003559a50e4e17fa036aad2a
Damjan <damjandimitrov1@gmail.com>
2020-03-29 20:59:03 +0200

    Fix: clear filters button

--- MVC/Models/Person.cs
+++ MVC/Models/Person.cs
@@ -6,10 +6,16 @@ namespace MVC.Models
     public class Person
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
         [DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
